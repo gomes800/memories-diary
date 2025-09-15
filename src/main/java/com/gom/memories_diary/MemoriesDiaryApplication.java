02f02c9e8146d4ae13a734c377439b1a59ab8266
gom-trab <jgomesleite8@gmail.com>
2025-09-15 10:57:28 -0300

    first commit

---
+++ src/main/java/com/gom/memories_diary/MemoriesDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.gom.memories_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriesDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoriesDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gom/memories_diary/MemoriesDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gom.memories_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriesDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
