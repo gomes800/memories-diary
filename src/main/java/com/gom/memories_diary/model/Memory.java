479f867160b070ed7ade097835719c6c3799ab3d
gom-trab <jgomesleite8@gmail.com>
2025-09-15 15:23:40 -0300

    feat: create entities user and memory

---
+++ src/main/java/com/gom/memories_diary/model/Memory.java
@@ -0,0 +1,33 @@
+package com.gom.memories_diary.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "memories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Memory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private LocalDateTime date;
+    private String content;
+    
+    @ElementCollection
+    @CollectionTable(name = "memory_photos", joinColumns = @JoinColumn(name = "memory_id"))
+    @Column(name = "photo_url")
+    private List<String> photoUrls = new ArrayList<>();
+
+    @ManyToMany
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+}
---
+++ src/main/java/com/gom/memories_diary/model/User.java
@@ -0,0 +1,26 @@
+package com.gom.memories_diary.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private List<Memory> memories = new ArrayList<>();
+}
